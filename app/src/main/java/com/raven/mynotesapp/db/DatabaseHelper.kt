9d1912d5b1aa3d94cbaf6543c70b6314316eedf0
Rois Faozi <67546196+Roisfaozi@users.noreply.github.com>
2020-08-25 13:27:27 +0700

    First commit

---
+++ app/src/main/java/com/raven/mynotesapp/db/DatabaseHelper.kt
@@ -0,0 +1,30 @@
+package com.raven.mynotesapp.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.raven.mynotesapp.db.DatabaseContract.NoteColumns.Companion.TABLE_NAME
+
+internal class DatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    companion object {
+        private const val DATABASE_NAME = "dbnoteapp"
+
+        private const val DATABASE_VERSION = 1
+
+        private val SQL_CREATE_TABLE_NOTE = "CRATE TABLE $TABLE_NAME" +
+                " (${DatabaseContract.NoteColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
+                " ${DatabaseContract.NoteColumns.TITLE} TEXT NO NULL," +
+                " ${DatabaseContract.NoteColumns.DESCRIPTION} TEXT NO NULL," +
+                " ${DatabaseContract.NoteColumns.DATE} TEXT NO NULL,)"
+    }
+
+    override fun onCreate(db : SQLiteDatabase) {
+        db.execSQL(SQL_CREATE_TABLE_NOTE)
+
+    }
+
+    override fun onUpgrade (db : SQLiteDatabase, oldVersion: Int, newVersion: Int){
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        onCreate(db)
+    }
+}
